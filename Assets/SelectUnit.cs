1002fc235d43e8141eef5040b7ad2ab4edd57082
William Ruppenthal <williamruppenthal42@gmail.com>
2015-04-14 09:34:48 -0700

    everything is kiill

--- Assets/SelectUnit.cs
+++ Assets/SelectUnit.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class SelectUnit : MonoBehaviour {
 
-	void Update () {
-		if (Input.GetMouseButtonDown(0))
-		{
-			RaycastHit hitInfo = new RaycastHit();
-			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-			if (hit) 
-			{
-				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
-				Destroy(hitInfo.transform.gameObject);
-			} else {
-				Debug.Log("No hit");
-			}
-		} 
-	}
+//	void Update () {
+//		if (Input.GetMouseButtonDown(0))
+//		{
+//			RaycastHit hitInfo = new RaycastHit();
+//			bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+//			if (hit) 
+//			{
+//				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+//
+//				HexChunk theChunk = hitInfo.transform.gameObject;
+//
+//
+//			} else {
+//				Debug.Log("No hit");
+//			}
+//		} 
+//	}
 }
